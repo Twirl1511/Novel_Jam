e98bc11369359cfdd4301d70901010949265e827
Twirl1511 <64476851+Twirl1511@users.noreply.github.com>
2021-02-25 20:53:57 +0300

    печатающийся текст

---
+++ Novel_Jam/Assets/Scripts/BackgroundScripts/ParallaxBackground.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour
+{
+    public float scrollX = .5f;
+    public float scrollY = .5f;
+    // Update is called once per frame
+    void Update()
+    {
+        float OffsetX = scrollX * Time.deltaTime;
+        float OffsetY = scrollY * Time.deltaTime;
+    }
+}
---
+++ Novel_Jam/Assets/Scripts/TextScripts/SlowTypingText.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SlowTypingText : MonoBehaviour
+{
+    public string fullText;
+    [SerializeField] private Text text;
+    [SerializeField] private float delay = .1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fullText = text.text;
+        StartCoroutine(TypeTexrWithDelay());
+    }
+
+    IEnumerator TypeTexrWithDelay()
+    {
+        float timeDelay;
+        for(int i = 0; i < fullText.Length; i++)
+        {
+            int randomDelay = Random.Range(3,6);
+            int randomModifier = Random.Range(2,5);
+            
+            if (i % randomDelay == 0)
+            {
+                timeDelay = delay * randomModifier;
+            }
+            else
+            {
+                timeDelay = delay;
+            }
+            yield return new WaitForSeconds(timeDelay);
+            text.text = fullText.Substring(0, i + 1);
+        }
+    }
+}
