6671eda0fc6df7e5251f679b4218fdfdbe690e6a
Twirl1511 <64476851+Twirl1511@users.noreply.github.com>
2021-02-27 12:36:05 +0300

    сделал стартовое меню

---
+++ Novel_Jam/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour
+{
+    [SerializeField] private Button StartButton;
+    [SerializeField] private Button ExitButton;
+    [SerializeField] private GameObject HeckAndPaper;
+    [SerializeField] private float seconds = 3f;
+
+    void Start()
+    {
+        
+    }
+
+    IEnumerator DelayStart(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void StartButtonClic()
+    {
+        StartButton.GetComponent<Button>().enabled = false;
+        HeckAndPaper.SetActive(true);
+        StartCoroutine(DelayStart(seconds));
+    }
+
+    public void ExittButtonClic()
+    {
+        Application.Quit();
+    }
+    
+}
