2f26121a3619e77e2b046f040e46812488855ff0
Twirl1511 <64476851+Twirl1511@users.noreply.github.com>
2021-02-28 00:36:58 +0300

    хз чет наворотил

---
+++ Novel_Jam/Assets/Scripts/DialogueScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueScript : MonoBehaviour
+{
+    [SerializeField] private Text text;
+    public PhraseAndTime[] HecksPhrases;
+    
+    private float timer;
+
+
+
+
+
+    void Start()
+    {
+        timer = 0;
+        StartCoroutine(DelayBtwPhrases());
+    }
+
+    private void Update()
+    {
+        
+        
+    }
+
+    IEnumerator DelayBtwPhrases()
+    {
+
+        foreach(var e in HecksPhrases)
+        {
+            text.text = e.text;
+            e.heckSprite.SetActive(true);
+            yield return new WaitForSeconds(e.time);
+            e.heckSprite.SetActive(false);
+        }
+
+        
+
+    }
+
+    
+}
---
+++ Novel_Jam/Assets/Scripts/PhraseAndTime.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class PhraseAndTime
+{
+    
+    public float time;
+    public string text;
+    public GameObject heckSprite;
+}
