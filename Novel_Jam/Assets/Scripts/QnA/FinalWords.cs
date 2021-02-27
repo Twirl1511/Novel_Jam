42517931eae10db947e7b6934d1c63bef7d66eae
Twirl1511 <64476851+Twirl1511@users.noreply.github.com>
2021-02-27 16:19:18 +0300

    финальный вопрос с тремя словами

---
+++ Novel_Jam/Assets/Scripts/QnA/FinalWords.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FinalWords : MonoBehaviour
+{
+    [SerializeField] private InputField firstWord;
+    [SerializeField] private InputField secondWord;
+    [SerializeField] private InputField thirdWord;
+    public string[] firstWordsArray;
+    public string[] secondWordsArray;
+    public string[] thirdWordsArray;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AnswerButton()
+    {
+        if (IsAnswerCorrect())
+        {
+            Debug.Log("Nice answer");
+        }
+        else
+        {
+            Debug.Log("Bad answer");
+        }
+    }
+
+    private bool FirstWordContained()
+    {
+        foreach(var e in firstWordsArray)
+        {
+            if(e == firstWord.text.ToLower().Trim() && e != "")
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private bool SecondWordContained()
+    {
+        foreach (var e in secondWordsArray)
+        {
+            if (e == secondWord.text.ToLower() && e != "")
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private bool ThirdWordContained()
+    {
+        foreach (var e in thirdWordsArray)
+        {
+            if (e == thirdWord.text.ToLower() && e != "")
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private bool IsAnswerCorrect()
+    {
+        if(FirstWordContained() && SecondWordContained() && ThirdWordContained())
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
---
+++ Novel_Jam/Assets/Scripts/QnA/QuestionA.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuestionA : MonoBehaviour
+{
+    [SerializeField] private InputField inputFied;
+    [SerializeField] private Text text;
+    [SerializeField] private Text fake;
+    [SerializeField] private GameObject ReactionRUS;
+    [SerializeField] private GameObject ReactionENG;
+    [SerializeField] private GameObject ReactionOTHER;
+    private Color defaultColor;
+    private Color transperentColor;
+    void Start()
+    {
+        defaultColor = new Color(text.color.r, text.color.g, text.color.b, text.color.a);
+        transperentColor = new Color(text.color.r, text.color.g, text.color.b, 0);
+    }
+
+    void Update()
+    {
+        
+        /// eng
+        if (text.text == "a")
+        {
+            fake.text = "ф";
+            text.color = transperentColor;
+        } 
+        if (text.text == "A")
+        {
+            fake.text = "Ф";
+            text.color = transperentColor;
+        }
+
+
+        // rus
+        if (text.text == "а")
+        {
+            fake.text = "f";
+            text.color = transperentColor;
+        }
+        if (text.text == "А")
+        {
+            fake.text = "F";
+            text.color = transperentColor;
+        }
+
+
+
+        if (text.text == "")
+        {
+            fake.text = "";
+            text.color = defaultColor;
+        }
+        else
+        {
+            inputFied.enabled = false;
+            if (_answerFlag)
+            {
+                Answer();
+            }
+            
+        }
+
+        
+
+    }
+
+
+    private bool _answerFlag = true;
+    public void Answer()
+    {
+        
+        if (text.text.ToLower() == "a")
+        {
+            Debug.Log(" eng A");
+            StaticResults.KeyboardAnswer = "eng";
+        }
+        else if (text.text.ToLower() == "а")
+        {
+            Debug.Log(" рус A");
+            StaticResults.KeyboardAnswer = "rus";
+        }
+        else
+        {
+            Debug.Log(" вообще другая буква");
+            StaticResults.KeyboardAnswer = "other";
+        }
+
+        _answerFlag = false;
+    }
+}
