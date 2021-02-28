ef0bcd7199c0c5b5b5320f115c1b65081a2dd708
Twirl1511 <64476851+Twirl1511@users.noreply.github.com>
2021-02-28 16:50:49 +0300

    пуш

---
+++ Novel_Jam/Assets/Scripts/FINAL/FinalManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalManager : MonoBehaviour
+{
+    [SerializeField] private GameObject[] backgrounds;
+    [SerializeField] private GameObject END;
+    void Start()
+    {
+        backgrounds[0].SetActive(true);
+        StartCoroutine(DelayNextBG(backgrounds[1],5));
+        StartCoroutine(DelayNextBG(backgrounds[2], 9));
+        StartCoroutine(DelayNextBG(backgrounds[3], 37));
+        StartCoroutine(DelayNextBG(backgrounds[4], 40));
+        StartCoroutine(DelayNextBG(END, 47));
+    }
+
+    IEnumerator DelayNextBG(GameObject bg, float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        bg.SetActive(true);
+    }
+    void Update()
+    {
+        
+    }
+}
---
+++ Novel_Jam/Assets/Scripts/PhraseAndTimeFinal.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class PhraseAndTimeFinal
+{
+    
+    public float time;
+    public string textName;
+    public string textDialogue;
+
+    
+}
